829367807c1ca717aa64ae18a847db3724cf957d
Guilherme Kayo Shida <guilhermeshida@gmail.com>
2019-11-13 19:54:32 -0300

    Modified Story property to private

--- lib/story.dart
+++ lib/story.dart
@@ -1,7 +1,14 @@
 class Story {
-  String storyTitle;
-  String choice1;
-  String choice2;
+  String _storyTitle;
+  String _choice1;
+  String _choice2;
 
-  Story({this.storyTitle, this.choice1, this.choice2});
+  Story({storyTitle, choice1, choice2})
+      : this._storyTitle = storyTitle,
+        this._choice1 = choice1,
+        this._choice2 = choice2;
+  
+  String get storyTitle => _storyTitle;
+  String get choice1 => _choice1;
+  String get choice2 => _choice2;
 }
